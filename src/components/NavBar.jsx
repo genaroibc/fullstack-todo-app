128c161845a5c4c86f33727e872e188a20fc4586
genaroibc <bonavitaismael@gmail.com>
2022-10-09 21:38:50 -0300

    add initial structure

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const STNav = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  position: sticky;
+  top: 0;
+
+  width: 100%;
+  padding: 1rem;
+
+  background-color: steelblue;
+`;
+
+export default function NavBar() {
+  return (
+    <STNav>
+      <Link href="/">Home</Link>
+      <Link href="/tasks">Tasks</Link>
+      <Link href="/tasks/create">Create Task</Link>
+      <Link href="/tasks/create">Update Task</Link>
+    </STNav>
+  );
+}
