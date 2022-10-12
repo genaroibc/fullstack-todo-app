4e317e34dff904f34b17a7644c3c5f097cc033a1
genaroibc <bonavitaismael@gmail.com>
2022-10-12 18:22:06 -0300

    fix: update env variables

--- src/services/updateTask.js
+++ src/services/updateTask.js
@@ -1,5 +1,5 @@
 export const updateTask = (task_id, task_data) => {
-  return fetch(`${process.env.VERCEL_URL}/${task_id}`, {
+  return fetch(`${process.env.NEXT_PUBLIC_TASKS_API_URL}/${task_id}`, {
     method: "PUT",
     body: JSON.stringify(task_data),
     headers: {
