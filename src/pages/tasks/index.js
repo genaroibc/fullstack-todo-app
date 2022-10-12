9786ce69ba201734dd5d3e2ceee3417b89a5c1eb
genaroibc <bonavitaismael@gmail.com>
2022-10-11 21:34:20 -0300

    feat: show confirmation before delete a task.

--- src/pages/tasks/index.js
+++ src/pages/tasks/index.js
@@ -1,17 +1,54 @@
 import { deleteTask } from "services/deleteTask";
 import { TasksList } from "./components/TasksList";
+import { Modal, Button } from "semantic-ui-react";
+import { useState } from "react";
 
 export default function TasksPage({ tasks = [] }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [taskIdToDelete, setTaskIdToDelete] = useState(null);
+
+  const closeModal = () => setModalIsOpen(false);
+  const openModal = () => setModalIsOpen(true);
+
   const handleDelete = async task_id => {
     const response = await deleteTask(task_id);
     if (!response.ok) console.error(response);
+
+    closeModal();
   };
 
   return (
     <main>
       <h1>Tasks list:</h1>
 
-      <TasksList tasks={tasks} handleDelete={handleDelete} />
+      <TasksList
+        tasks={tasks}
+        handleDelete={task_id => {
+          setTaskIdToDelete(task_id);
+          openModal();
+        }}
+      />
+
+      <Modal open={modalIsOpen}>
+        <Modal.Content>
+          <Modal.Description>
+            Are you sure you want to delete this task? This action is
+            permanent!!
+          </Modal.Description>
+          <Modal.Actions>
+            <Button color={"facebook"} onClick={closeModal}>
+              Cancel
+            </Button>
+
+            <Button
+              color={"youtube"}
+              onClick={() => handleDelete(taskIdToDelete)}
+            >
+              Delete
+            </Button>
+          </Modal.Actions>
+        </Modal.Content>
+      </Modal>
 
       {tasks.lenght && <h3>No tasks yet...</h3>}
     </main>
