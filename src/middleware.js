1360ac394eabfa1c0c440bfbd12c7889f8263dc2
genaroibc <bonavitaismael@gmail.com>
2022-11-08 14:33:25 -0300

    feat: add pages middleware

---
+++ src/middleware.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+
+export async function middleware(req) {
+  const { cookies, url } = req;
+
+  const authToken = cookies.get("userAuthToken");
+
+  if (authToken === undefined) {
+    const urlToRedirect = new URL("/login", url);
+
+    return NextResponse.redirect(urlToRedirect);
+  }
+
+  try {
+    await jwtVerify(authToken, new TextEncoder().encode("secret"));
+
+    return NextResponse.next();
+  } catch (error) {
+    console.error(error);
+
+    const urlToRedirect = new URL("/login", url);
+    return NextResponse.redirect(urlToRedirect);
+  }
+}
+
+export const config = {
+  matcher: ["/tasks/:path*", "/dashboard"]
+};
