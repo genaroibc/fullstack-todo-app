6ceeb60f15d765f298a95189986b3f1936094803
genaroibc <bonavitaismael@gmail.com>
2022-11-08 14:35:32 -0300

    feat: show whether user is logged or not

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,19 @@
+import { useAuthContext } from "context/AuthContext";
 import Link from "next/link";
 import { Header } from "semantic-ui-react";
 
 export default function NavBar() {
+  const { auth } = useAuthContext();
+
+  const userMessage = auth.isAuth
+    ? `Welcome ${auth.username}`
+    : "Welcome guest";
+
   return (
     <Header
       style={{
         display: "flex",
-        justifyContent: "space-evenly",
+        justifyContent: "space-evenly"
       }}
       attached={"bottom"}
       inverted={true}
@@ -15,12 +22,30 @@ export default function NavBar() {
       <Link style={{ margin: "1rem " }} href="/">
         Home
       </Link>
+      <Link style={{ margin: "1rem " }} href="/dashboard">
+        Dashboard
+      </Link>
+      <Link style={{ margin: "1rem " }} href="/login">
+        Login
+      </Link>
       <Link style={{ margin: "1rem " }} href="/tasks">
         Tasks
       </Link>
       <Link style={{ margin: "1rem " }} href="/tasks/create">
         Create Task
       </Link>
+
+      <div>
+        <span
+          style={{
+            backgroundColor: "steelblue",
+            borderRadius: "50%",
+            color: "honeydew"
+          }}
+        >
+          {userMessage}
+        </span>
+      </div>
     </Header>
   );
 }
