ac4bf8dc7026f2b78822cc7b586b326db9ec5139
genaroibc <bonavitaismael@gmail.com>
2022-11-16 12:10:14 -0300

    feat: implement RegisterForm

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,49 +1,5 @@
-import { LoginForm } from "components/LoginForm";
-import { useAuthContext } from "context/AuthContext";
-import { useRouter } from "next/router";
+import { RegisterForm } from "components/RegisterForm";
 
-const NAME_INPUT_NAME = "user-name";
-const PASSWORD_INPUT_NAME = "user-password";
-
-const INPUTS_CONFIG = [
-  {
-    type: "text",
-    name: NAME_INPUT_NAME,
-    placeholder: "Enter your username"
-  },
-  {
-    type: "password",
-    name: PASSWORD_INPUT_NAME,
-    placeholder: "Enter your password"
-  }
-];
 export default function Login() {
-  const { setAuth } = useAuthContext();
-  const handleSubmit = async e => {
-    e.preventDefault();
-
-    const username = e.target[NAME_INPUT_NAME].value;
-    const password = e.target[PASSWORD_INPUT_NAME].value;
-
-    const response = await fetch("/api/auth/login", {
-      method: "POST",
-      body: JSON.stringify({
-        username,
-        password
-      })
-    });
-    const data = await response.json();
-
-    console.log({ data });
-    setAuth({ isAuth: true, username: data.username });
-  };
-  return (
-    <>
-      <LoginForm
-        handleSubmit={handleSubmit}
-        submitButtonText="Log In"
-        inputsConfig={INPUTS_CONFIG}
-      />
-    </>
-  );
+  return <RegisterForm type="login" />;
 }
