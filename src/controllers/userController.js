c8faea021b7bba959e1e3ff13e8f99d54036f604
genaroibc <bonavitaismael@gmail.com>
2022-11-16 12:12:11 -0300

    add UserController

---
+++ src/controllers/userController.js
@@ -0,0 +1,71 @@
+import { UserModel } from "models/UserModel";
+
+export async function getOneUserById(id) {
+  try {
+    const user = await UserModel.findById(id);
+
+    return user || {};
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function getOneUserByUserName(username) {
+  try {
+    const user = await UserModel.findOne({ username });
+    return user || {};
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function getTasksByUserId(userId) {
+  try {
+    const user = (await UserModel.findById(userId)) ?? {};
+
+    return user.tasks || [];
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function getAllUsers() {
+  try {
+    const users = await UserModel.find();
+
+    return users || [];
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function createOneUser(userData) {
+  try {
+    const newUser = new UserModel(userData);
+
+    await newUser.save();
+
+    return newUser || {};
+  } catch (error) {
+    console.error({ error });
+    return error;
+  }
+}
+
+export async function createTaskByUserId({
+  taskData: { title, description } = {},
+  userId = ""
+}) {
+  try {
+    const user = await UserModel.findById(userId);
+
+    user.tasks.push({ title, description });
+
+    const savedUser = await user.save();
+
+    return savedUser || {};
+  } catch (error) {
+    console.error({ error });
+    return error;
+  }
+}
