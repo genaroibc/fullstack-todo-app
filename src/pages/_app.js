64d0ecfae1c0db9dfcded962f0410edf662fa9ea
genaroibc <bonavitaismael@gmail.com>
2022-11-16 12:10:57 -0300

    feat: implement AuthContextProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,15 @@
 import NavBar from "components/NavBar";
+import { AuthContextProvider } from "context/AuthContext";
 import "semantic-ui-css/semantic.min.css";
 import "styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <AuthContextProvider>
       <NavBar />
 
       <Component {...pageProps} />
-    </>
+    </AuthContextProvider>
   );
 }
 
