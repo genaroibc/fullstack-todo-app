6b457f4211c15db7dd383ce6f15c42cc1178c304
genaroibc <bonavitaismael@gmail.com>
2022-11-16 12:05:48 -0300

    feat: add API tasks endpoint

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -1,25 +1,40 @@
-import { createOneTask, getAllTasks } from "controllers/taskController";
-
 import { mongoDBConnection } from "services/mongoDBConnection";
+import jwt from "jsonwebtoken";
+import {
+  createTaskByUserId,
+  getTasksByUserId
+} from "controllers/userController";
 
 mongoDBConnection(process.env.MONGODB_URI);
 
 export default async function handler(req, res) {
-  const { method, body } = req;
+  const { method, body, cookies } = req;
+
+  const { userAuthToken } = cookies;
+
+  if (!userAuthToken) {
+    return res
+      .status(403)
+      .json({ error: true, error_message: "Invalid token" });
+  }
+
+  const { userId } = jwt.verify(userAuthToken, "secret");
 
   switch (method) {
     case "GET":
-      const tasks = await getAllTasks();
+      const tasks = await getTasksByUserId(userId);
+
       return res.status(200).json(tasks);
 
     case "POST":
-      const createdTask = await createOneTask(body);
+      const createdTask = await createTaskByUserId({ taskData: body, userId });
+
       return res.status(201).json(createdTask);
 
     default:
-      return res.status(400).json({
-        error_code: 400,
-        error_mesage: "Invalid request: this method is not supported",
+      return res.status(405).json({
+        error_code: 405,
+        error_mesage: "Invalid request: method not supported on this resource."
       });
   }
 }
